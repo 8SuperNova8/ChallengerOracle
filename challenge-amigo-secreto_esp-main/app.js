87b3e0751f85e0a338aaaa064af639c0ce5dc37b
Verónica Sierra <vssombre@outlook.com>
2025-02-03 23:51:16 -0500

    AS se modifica la funcion para agregar la lista como etiqueta <li> en html

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -2,51 +2,67 @@ let friends = [];
 let cont = 0;
 
 //agrega Amigo a la lista 
-function addFriend(){
-    let inputText = document.getElementById("amigo").value;
+function addFriend() {
+    let inputText = document.getElementById("amigo").value.trim(); //trim elimina espacios
     //retira el resultado si ya se ha ejecutado Sortear amigo
-    if (cont >0) showOnScreen("resultado", "")
-    if (inputText != ""){
-        friends.push(inputText)
-        clearBox();
-        showOnScreen("listaAmigos", friends.join("\n"))
-        
-    }else{
+    if (cont > 0) {
+        clearShowOnScreen("resultado", "")
+        cont = 0;
+    }
+    if (inputText != "") {
+        //si no existe se agrega
+        if (!friends.includes(inputText)) {
+
+            friends.push(inputText)
+            clearBox();
+            showOnScreen("listaAmigos", inputText)
+        } else { alert("este amigo ya esta en la lista") }
+    } else {
         alert("Ingresa un valor valido")
     }
 }
 
 //selecciona un amigo aleatorio
-function ramdonFriend(){
-    if (friends.length!= 0) {
-    cont ++
-    let resultRamdon = Math.floor(Math.random()* friends.length)
-    let secretFriend = friends[resultRamdon]
-    //muestra en la etiqueta Ul el resultado del random
-    showOnScreen("resultado", `El Amigo secreto es:\n ${secretFriend}`)
-    start()
-    console.log (friends)
-    }else{
-        alert("debe ingresar Amigos para Sortear")
+function ramdonFriend() {
+    if (friends.length != 0) {
+        cont++
+        const resultRamdon = Math.floor(Math.random() * friends.length)
+        const secretFriend = friends[resultRamdon]
+        //muestra en la etiqueta Ul el resultado del random
+        showOnScreen("resultado", `El Amigo secreto es:\n ${secretFriend}`)
+        start()
+    } else {
+        alert("ingresa un Nuevo grupo de Amigos para Sortear")
     }
-    
 }
 
 //limpiar caja 
-function clearBox(){
-    return document.querySelector(".input-name").value ="";
+function clearBox() {
+    return document.querySelector(".input-name").value = "";
 }
 
-//showOnScreen muestra en pantalla
-function showOnScreen(etiqueta, contenido){
-    return document.getElementById(etiqueta).innerText= contenido;
+//muestra en pantalla agregando etiquetas en <ul>
+function showOnScreen(id, contenido) {
+    //llamo la etiqueta donde quiero ingresar la etiqueta li que es la <ul>
+    const ul = document.getElementById(id);
+    //Creo la etiqueta li
+    const li = document.createElement("li");
+    //asigno el texto a la etiqueta
+    li.appendChild(document.createTextNode(contenido));
+    ul.appendChild(li);
 }
 
-function start (){
+//borra el contenido que muestra en pantalla
+function clearShowOnScreen(id, contenido) {
+    return document.getElementById(id).innerText = contenido;
+}
+
+//reinicia variables
+function start() {
     //se inicia la lista
     friends = []
     //se limpiar la etiqueta UL
-    showOnScreen("listaAmigos", "")
+    clearShowOnScreen("listaAmigos", "")
 }
 
 
